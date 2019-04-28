dd4479c1b7b3e6814dcf33a4a0882326a29b479e
Jayjeet Chakraborty <jc.github@rediffmail.com>
2019-04-28 23:03:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ lib = cdll.LoadLibrary("./main.so")
 class GoString(Structure):
     _fields_ = [("p", c_char_p), ("n", c_longlong)]
 
-path = '/home/jayjeet/Videos/popper/ci/cli.workflow'
+path = "/home/jayjeet/Videos/popper/ci/cli.workflow"
 lib.Validate.argtypes = [GoString]
 lib.Validate(GoString(path.encode("utf-8"), len(path)))
